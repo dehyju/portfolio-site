fcd81eca0c272c3d9141ed1e25460b6b05aaf8ff
Stephen <stephen.t.j.leong@gmail.com>
2025-11-18 22:02:04 +0000

    fix: remove location prop from Routes component

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <>
-      <Routes location="/">
+      <Routes>
         <Route index element={<ComingSoon />} />
         {/* {<Route path="/contact" element={<Contact />} />} */}
         <Route path='*' element={<Navigate to="/" replace />} />
